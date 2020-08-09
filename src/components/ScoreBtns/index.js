a563005d6b5415b82995666c2f93d509394ed2eb
Jesper <jesper.lundqvist@gmail.com>
2020-08-09 13:32:02 +0200

    Small css adjustment

--- src/components/ScoreBtns/index.js
+++ src/components/ScoreBtns/index.js
@@ -5,13 +5,12 @@ import styled from 'styled-components';
 const ScoreBtnsStyled = styled.div`
   display: flex;
   padding: 20px 15px;
-  min-height: 80px;
   justify-content: space-between;
   button {
     font-size: 24px;
     background-color: var(--grey-color);
     width: 75px;
-    height: 100%;
+    height: 50px;
   }
 `;
 
